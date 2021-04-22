d01a468a4c7df8bd547cdf0ee1a46bc0d177e3c9
icarly <carlymarie.07@gmail.com>
2021-04-21 23:46:57 -0400

    stable switching

--- src/modules/lunch-generator.js
+++ src/modules/lunch-generator.js
@@ -16,49 +16,19 @@ export class ResturauntChoice extends Resturaunt {
 
 // Store Fast food options in an array
 const fastFood = [
-  new ResturauntChoice(
-    "Chick Fli A",
-    "252 Bumby Road",
-    "(845) 645-7787",
-    "*****"
-  ),
-  new ResturauntChoice("Burger King", "898 Load Road", "(998) 645-7787", "***"),
-  new ResturauntChoice("Zaxbys", "8987 Some Road", "(998) 894-7787", "****"),
-  new ResturauntChoice("McDonalds", "8987 Golden Road", "(788) 895-7787", "**"),
-  new ResturauntChoice(
-    "Whataburger",
-    "434 Space Road",
-    "(889) 895-9785",
-    "****"
-  ),
+  new ResturauntChoice( "Chick Fli A", "252 Bumby Road", "(845) 645-7787", "*****" ),
+  new ResturauntChoice( "Burger King", "898 Load Road", "(998) 645-7787", "***" ),
+  new ResturauntChoice( "Zaxbys", "8987 Some Road", "(998) 894-7787", "****" ),
+  new ResturauntChoice( "McDonalds", "8987 Golden Road", "(788) 895-7787", "**" ),
+  new ResturauntChoice( "Whataburger", "434 Space Road", "(889) 895-9785", "****" )
 ];
 
 // Store sit down food options
 const sitDownFood = [
-  new ResturauntChoice(
-    "Mom & Pops",
-    "454 Bumby Road",
-    "(884) 645-4845",
-    "****"
-  ),
-  new ResturauntChoice(
-    "River Side",
-    "898 River Way",
-    "(998) 645-7444",
-    "*****"
-  ),
-  new ResturauntChoice(
-    "Silver Slipper",
-    "373 Slider Way",
-    "(989) 894-7889",
-    "**"
-  ),
-  new ResturauntChoice(
-    "Bone Foxes",
-    "8987 Golden Road",
-    "(788) 895-7787",
-    "**"
-  ),
+  new ResturauntChoice( "Mom & Pops", "454 Bumby Road", "(884) 645-4845", "****" ),
+  new ResturauntChoice( "River Side", "898 River Way", "(998) 645-7444", "*****" ),
+  new ResturauntChoice( "Silver Slipper", "373 Slider Way", "(989) 894-7889", "***" ),
+  new ResturauntChoice( "Bone Foxes", "8987 Golden Road", "(788) 895-7787","**" ),
   new ResturauntChoice("Real Soul", "4344 Soul Road", "(889) 895-9785", "****"),
 ];
 
@@ -86,13 +56,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
   // show fast food result
   fastBtn.addEventListener("click", function () {
     shuffle(fastFood);
-    output.innerHTML = fastFood[0].name;
+    output.innerHTML = "<h1>" + fastFood[0].name + "</h1><h3>" + fastFood[0].location + "</h3><a href='tel:" + fastFood[0].number + "'>" + fastFood[0].number + "</a><h6>Rating: " + fastFood[0].rating + "</h6>";
   });
 
   // show sit-down result
   sitBtn.addEventListener("click", function () {
     shuffle(sitDownFood);
-    output.innerHTML = sitDownFood[0].name;
+    output.innerHTML = "<h1>" + sitDownFood[0].name + "</h1><h3>" + sitDownFood[0].location + "</h3><a href='tel:" + sitDownFood[0].number + "'>" + sitDownFood[0].number + "</a><h6>Rating: " + sitDownFood[0].rating + "</h6>";
   });
 });
 
