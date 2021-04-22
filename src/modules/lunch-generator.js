12c17711e4acd33958666f2025a3554414ccbf9c
icarly <carlymarie.07@gmail.com>
2021-04-21 23:30:55 -0400

    stable functioning

--- src/modules/lunch-generator.js
+++ src/modules/lunch-generator.js
@@ -1,5 +1,5 @@
 export default class Resturaunt {
-  constructor( name, location, number ) {
+  constructor(name, location, number) {
     this.name = name;
     this.location = location;
     this.number = number;
@@ -8,7 +8,7 @@ export default class Resturaunt {
 
 //sub class
 export class ResturauntChoice extends Resturaunt {
-  constructor( name, location, number, rating ) {
+  constructor(name, location, number, rating) {
     super(name, location, number);
     this.rating = rating;
   }
@@ -16,20 +16,50 @@ export class ResturauntChoice extends Resturaunt {
 
 // Store Fast food options in an array
 const fastFood = [
-  new ResturauntChoice( 'Chick Fli A', '252 Bumby Road', '(845) 645-7787', '*****' ),
-  new ResturauntChoice( 'Burger King', '898 Load Road', '(998) 645-7787', '***' ),
-  new ResturauntChoice( 'Zaxbys', '8987 Some Road', '(998) 894-7787', '****' ),
-  new ResturauntChoice( 'McDonalds', '8987 Golden Road', '(788) 895-7787', '**' ),
-  new ResturauntChoice( 'Whataburger', '434 Space Road', '(889) 895-9785', '****' )
+  new ResturauntChoice(
+    "Chick Fli A",
+    "252 Bumby Road",
+    "(845) 645-7787",
+    "*****"
+  ),
+  new ResturauntChoice("Burger King", "898 Load Road", "(998) 645-7787", "***"),
+  new ResturauntChoice("Zaxbys", "8987 Some Road", "(998) 894-7787", "****"),
+  new ResturauntChoice("McDonalds", "8987 Golden Road", "(788) 895-7787", "**"),
+  new ResturauntChoice(
+    "Whataburger",
+    "434 Space Road",
+    "(889) 895-9785",
+    "****"
+  ),
 ];
 
 // Store sit down food options
 const sitDownFood = [
-  new ResturauntChoice( 'Mom & Pops', '454 Bumby Road', '(884) 645-4845', '****' ),
-  new ResturauntChoice( 'River Side', '898 River Way', '(998) 645-7444', '*****' ),
-  new ResturauntChoice( 'Silver Slipper', '373 Slider Way', '(989) 894-7889', '**' ),
-  new ResturauntChoice( 'Bone Foxes', '8987 Golden Road', '(788) 895-7787', '**' ),
-  new ResturauntChoice( 'Real Soul', '4344 Soul Road', '(889) 895-9785', '****' )
+  new ResturauntChoice(
+    "Mom & Pops",
+    "454 Bumby Road",
+    "(884) 645-4845",
+    "****"
+  ),
+  new ResturauntChoice(
+    "River Side",
+    "898 River Way",
+    "(998) 645-7444",
+    "*****"
+  ),
+  new ResturauntChoice(
+    "Silver Slipper",
+    "373 Slider Way",
+    "(989) 894-7889",
+    "**"
+  ),
+  new ResturauntChoice(
+    "Bone Foxes",
+    "8987 Golden Road",
+    "(788) 895-7787",
+    "**"
+  ),
+  new ResturauntChoice("Real Soul", "4344 Soul Road", "(889) 895-9785", "****"),
 ];
 
 // shuffle array
@@ -43,27 +73,37 @@ shuffle(fastFood);
 // test array shuffle
 console.log(sitDownFood[0].name);
 
-// store buttons
-const fastBtn = document.querySelector("#fast-food");
-const sitBtn = document.querySelector("#sit-dow");
-const output = document.querySelector('#output');
+/* Wait until DOM ready to query elements */
+document.addEventListener("DOMContentLoaded", (event) => {
+  /* Add results DOM elements */
+  document.body.insertAdjacentHTML("beforeend", foodResults);
 
-// show fast food result 
-fastBtn.addEventListener("click", function() {
-  output.innerHTML = foodResults;
+  // store buttons
+  const fastBtn = document.querySelector("#fast-food");
+  const sitBtn = document.querySelector("#sit-down");
+  const output = document.querySelector("#output");
+
+  // show fast food result
+  fastBtn.addEventListener("click", function () {
+    shuffle(fastFood);
+    output.innerHTML = fastFood[0].name;
+  });
+
+  // show sit-down result
+  sitBtn.addEventListener("click", function () {
+    shuffle(sitDownFood);
+    output.innerHTML = sitDownFood[0].name;
+  });
 });
-  
+
 const foodResults = `
   <div class="results">
     <div class="container">
       <div class="row justify-content-center align-item-center">
-
         <div class="col-12 col-md-6">
-          <div id="output">${fastFood[0].name}</div>
+          <div id="output"></div>
         </div>
-
       </div>
     </div>
   </div>
 `;
-
